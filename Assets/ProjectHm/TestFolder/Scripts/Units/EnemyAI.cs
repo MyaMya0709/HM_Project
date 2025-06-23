08f2d9b390f825922b9eeed99afd585349f18f88
MyaMya0709 <chlehdgh96@gmail.com>
2025-06-23 13:51:14 +0900

    [Fix] 적 Collider2D 반응 수정

--- Assets/ProjectHm/TestFolder/Scripts/Units/EnemyAI.cs
+++ Assets/ProjectHm/TestFolder/Scripts/Units/EnemyAI.cs
@@ -41,6 +41,8 @@ public class EnemyAI : UnitBase
 
     protected override void Dead()
     {
+        //Collider2D collider2D = GetComponent<Collider2D>();
+        //collider2D.enabled = false;
         base.Dead();
         // 이펙트나 드랍 추가 가능
     }
