ec78b830a4dc68dfa6091a691fd537fef90b541e
MyaMya0709 <chlehdgh96@gmail.com>
2025-06-24 18:05:11 +0900

    [Wip] 더블탭 대시 로직 임시 저장

--- Assets/ProjectHm/TestFolder/Scripts/Units/PlayerController.cs
+++ Assets/ProjectHm/TestFolder/Scripts/Units/PlayerController.cs
@@ -8,11 +8,12 @@ public class PlayerController : UnitBase
 {
     public Rigidbody2D rb;
     public Vector2 moveInput;
+    public bool isMove = false;
 
     [Header("Desh")]
     public float dashPower = 10f;
-    public float dashDuration = 5f;
     public float dashCooldown = 0.05f;
+    public float dashDistance = 20f;
     public bool isDashing;
     public bool isAbleDash = true;
     public Vector2 dashDirection;
@@ -26,9 +27,9 @@ public class PlayerController : UnitBase
     public int jumpCount = 0;
     public bool isJumpDash = false;
 
-    //[Header("DoubleTap")]
-    //float lastTapTime = -1f;               // 첫번째 입력 시간
-    //float doubleTapThreshold = 0.2f;       // 더블탭으로 인식하는 시간
+    [Header("DoubleTap")]
+    float lastTapTime = -1f;               // 첫번째 입력 시간
+    float doubleTapThreshold = 0.2f;       // 더블탭으로 인식하는 시간
 
     public IWeapon currentWeapon;
 
@@ -48,14 +49,11 @@ public class PlayerController : UnitBase
     {
         if (IsDead) return;
 
-        if (isJumpDash)
-            return; // 슈퍼 점프 중에는 다른 물리 계산 안 함
+        if (isJumpDash) return; // 슈퍼 점프 중에는 다른 물리 계산 안 함
 
-        if (isDashing)
-        {
-            rb.linearVelocity = dashDirection * dashPower;
-        }
-        else
+        if (isDashing) return;
+
+        if (isMove)
         {
             rb.linearVelocity = new Vector2(moveInput.x * stats.moveSpeed, rb.linearVelocity.y);
         }
@@ -64,41 +62,37 @@ public class PlayerController : UnitBase
     public void OnMove(InputAction.CallbackContext context)
     {
 
-        if (context.performed || context.canceled)
+        if (context.performed)
         {
-            Debug.Log("Move");
             float currentTime = Time.time;
-            moveInput = context.ReadValue<Vector2>();
 
-            // 방향이 바뀌면 마지막에 바라본 방향으로 갱신
-            if (Mathf.Abs(moveInput.x) > 0.01f)
-            {
-                lastLookDirection = new Vector2(Mathf.Sign(moveInput.x), 0);
-            }
-        }
-    }
-
-    public void OnDash(InputAction.CallbackContext context)
-    {
-        //더블탭 입력 확인
-        if (context.performed && context.interaction is MultiTapInteraction tap && tap.tapCount == 2)
-        {
-            Debug.Log("Double Tap Detected");
-
-            //대쉬 가능 여부 확인
-            if (!isDashing && isAbleDash)
+            //더블 탭 체크
+            if (currentTime - lastTapTime < doubleTapThreshold && !isDashing && isAbleDash)
             {
                 Debug.Log("DefaultDesh");
-                Vector2 dashDir;
-
-                // 대쉬 방향 선택
-                if (Mathf.Abs(moveInput.x) > 0.01f)
-                    dashDir = new Vector2(moveInput.x, 0).normalized;
-                else
-                    dashDir = lastLookDirection;
-
-                StartCoroutine(StartDash(dashDir));
+                StartCoroutine(StartDash(lastLookDirection));
+                lastTapTime = -1f; // 리셋
             }
+
+            // 일반 이동 로직
+            else
+            {
+                Debug.Log("Move");
+                isMove = true;
+                moveInput = context.ReadValue<Vector2>();
+
+                // 방향이 바뀌면 마지막에 바라본 방향으로 갱신
+                if (Mathf.Abs(moveInput.x) > 0.01f)
+                {
+                    lastLookDirection = new Vector2(Mathf.Sign(moveInput.x), 0);
+                }
+
+                lastTapTime = currentTime;
+            }
+        }
+        if (context.canceled)
+        {
+            isMove = false;
         }
     }
 
@@ -108,16 +102,24 @@ public class PlayerController : UnitBase
         isDashing = true; // isDashing 동안 사용자의 입력을 받지 않음
         isAbleDash = false;
         dashDirection = direction.normalized;
+        Vector2 curPos = gameObject.transform.position;
+        Vector2 tarPos = new Vector2(curPos.x + dashDistance, curPos.y);
 
+        rb.linearVelocity = Vector2.zero;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
 
         // 애니메이션 트리거
         //animator?.SetTrigger("Dash");
 
-        //rb.AddForce(dashDirection * dashPower, ForceMode2D.Impulse);
+        rb.linearVelocity = dashDirection * dashPower; //등속운동 시작
 
-        yield return new WaitForSeconds(dashDuration);
+        // 대쉬 거리까지 등속 운동 유지
+        while (curPos != tarPos)
+        {
+            rb.linearVelocity = dashDirection * dashPower;
+            yield return null; // 매 프레임 유지
+        }
 
         isDashing = false;
 
@@ -135,7 +137,7 @@ public class PlayerController : UnitBase
     public void OnJump(InputAction.CallbackContext context)
     {
 
-        if (context.performed)
+        if (context.started)
         {
             Debug.Log("Jump");
             float currentTime = Time.time;
@@ -146,40 +148,31 @@ public class PlayerController : UnitBase
                 jumpCount = 0;
             }
 
-            if (jumpCount <= 0)
+            //공중에서 더블 탭
+            if (!IsGrounded() && currentTime - lastTapTime < doubleTapThreshold)
+            {
+                Debug.Log("Double Tap Detected!");
+                StartCoroutine(SuperJump());
+                jumpCount++;
+                lastTapTime = -1f; // 리셋
+            }
+
+            // 일반 점프 로직
+            else if (jumpCount <= 0)
             {
                 Debug.Log("Single Tap");
                 // 2단 점프 직전에 y속도를 0으로 초기화
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
                 jumpCount++;
+                lastTapTime = currentTime;
             }
-                
-            ////공중에서 더블 탭
-            //if (!IsGrounded() && currentTime - lastTapTime < doubleTapThreshold)
-            //{
-            //    Debug.Log("Double Tap Detected!");
-            //    StartCoroutine(SuperJump());
-            //    jumpCount++;
-            //    lastTapTime = -1f; // 리셋
-            //}
-
-            //// 일반 점프 로직
-            //else if (jumpCount <= 1)
-            //{
-            //    Debug.Log("Single Tap");
-            //    // 2단 점프 직전에 y속도를 0으로 초기화
-            //    rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
-            //    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            //    jumpCount++;
-            //    lastTapTime = currentTime;
-            //}
         }
     }
 
     public void OnSuperJump(InputAction.CallbackContext context)
     {
-        if (context.performed && context.interaction is MultiTapInteraction tap && tap.tapCount == 2)
+        if (context.performed)
         {
             Debug.Log("Double Tap Detected");
             if(!IsGrounded() && !isDashing && isAbleDash)
@@ -197,7 +190,7 @@ public class PlayerController : UnitBase
         rb.gravityScale = 0.1f;
         rb.linearVelocity = Vector2.zero;
 
-        yield return new WaitForFixedUpdate(); // Vector2.zero 후 AddForce적용 시 한 프레임 쉬어야 잘됨
+        rb.linearVelocity = Vector2.up * dashPower; // 등속 운동 시작
         rb.AddForce(Vector2.up * dashPower, ForceMode2D.Impulse);
         //animator.Play("Jump", -1, 0);
 
@@ -220,7 +213,7 @@ public class PlayerController : UnitBase
             if (!IsGrounded()) // 공중에서 공격시
             {
                 // 위쪽 반동 추가
-                rb.linearVelocity = Vector2.up * 4f;
+                rb.linearVelocity = Vector2.up * 3f;
 
                 animator?.SetTrigger("Attack");
                 // 무기공격 로직 연결 예정
@@ -238,14 +231,14 @@ public class PlayerController : UnitBase
     public void OnDownAttack(InputAction.CallbackContext context)
     {
         //더블탭 입력 확인
-        if (context.performed && context.interaction is MultiTapInteraction tap && tap.tapCount == 2)
+        if (context.performed)
         {
             Debug.Log("Double Tap Detected");
 
             //빠른 하강
             if (!IsGrounded() && !isDashing && isAbleDash) //공중 + 대쉬 가능 여부
             {
-                Debug.Log("DownDesh");
+                Debug.Log("StartCoroutine");
                 StartCoroutine(StartDownAttack());
             }
         }
