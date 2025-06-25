2d1fdd97c268f51e339556fc2307897bb36de98d
MyaMya0709 <chlehdgh96@gmail.com>
2025-06-25 17:36:42 +0900

    [Feat] 카메라 이동 추가

---
+++ Assets/ProjectHm/TestFolder/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // 따라갈 2D 오브젝트
+    public float smoothSpeed = 0.125f; // 부드러운 이동 속도
+    public Vector3 offset; // 카메라와 목표 사이의 거리 offset
+
+    void FixedUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
