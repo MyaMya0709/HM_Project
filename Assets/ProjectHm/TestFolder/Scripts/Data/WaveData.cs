de80f324d1b1a781badfeab5311f664b2f4c6e63
MyaMya0709 <chlehdgh96@gmail.com>
2025-07-03 16:28:41 +0900

    [Chore] WaveData 불필요 변수 삭제

--- Assets/ProjectHm/TestFolder/Scripts/Data/WaveData.cs
+++ Assets/ProjectHm/TestFolder/Scripts/Data/WaveData.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/Wave")]
 public class WaveData : ScriptableObject
 {
-    public GameObject[] enemyPrefabs;    // 적의 정보가 들어갈 오브젝트
     public EnemyData[] spawnEnemyList;   // 적의 종류
     public int enemyCount = 9999;
     public float spawnInterval = 1f;
