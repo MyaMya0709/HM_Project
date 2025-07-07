bdb047cae6bbf2d32ee64dcf1179c3979e3dded2
MyaMya0709 <chlehdgh96@gmail.com>
2025-07-07 13:22:56 +0900

    [Wip] 임시저장

--- Assets/ProjectHm/TestFolder/Scripts/UI/DamageLabel.cs
+++ Assets/ProjectHm/TestFolder/Scripts/UI/DamageLabel.cs
@@ -1,16 +1,30 @@
+using TMPro;
 using UnityEngine;
 
 public class DamageLabel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("DamageLabel")]
+    public TMP_Text damageText;
+    public float fontSize = 10f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Anim easing")]
+    public AnimationCurve easeCurve;
+    private float _displayDuration;
+
+    [Header("Bezier curve settings")]
+    public Vector2 highPointOffset = new Vector2(-350, 300);
+    public Vector2 lowPointOffset = new Vector2(-100, -500);
+    public float heightVariationMax = 150;
+    public float heightVariationMin = 50;
+
+    private Vector3 _highPointOffsetBasedOnDir = Vector3.zero;
+    private Vector3 _dropPointOffsetBasedOnDir = Vector3.zero;
+    private bool _dir = true;
+
+    [Header("Visualize")]
+    public bool disPlayGizmos;
+    public int gizmoResolution = 20;
+    private Vector3 startingPositionForVisualization = Vector3.zero;
+
+    
 }
