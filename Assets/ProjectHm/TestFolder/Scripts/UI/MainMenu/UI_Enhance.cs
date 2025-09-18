005a38c0d08de6d1362a1a56ea959ded423414d5
MyaMya0709 <chlehdgh96@gmail.com>
2025-09-18 15:49:45 +0900

    [Wip] UI 임시 저장

--- Assets/ProjectHm/TestFolder/Scripts/UI/MainMenu/UI_Enhance.cs
+++ Assets/ProjectHm/TestFolder/Scripts/UI/MainMenu/UI_Enhance.cs
@@ -40,46 +40,38 @@ public class UI_Enhance : MonoBehaviour
     [Header("AttackTap")]
     [SerializeField] private RectTransform attackTap;
     [SerializeField] private TMP_Text AT_AttackDamage_TMP;
-    [SerializeField] private TMP_Text AT_KnockbackPower_TMP;
-    [SerializeField] private TMP_Text AT_AirbornePower_TMP;
-    [SerializeField] private TMP_Text AT_StunDuration_TMP;
-    [SerializeField] private TMP_Text AT_SlowDuration_TMP;
-    [SerializeField] private TMP_Text AT_SlowDecrease_TMP;
+    [SerializeField] private TMP_Text AT_Knockback_TMP;
+    [SerializeField] private TMP_Text AT_Airborne_TMP;
+    [SerializeField] private TMP_Text AT_Stun_TMP;
+    [SerializeField] private TMP_Text AT_Slow_TMP;
     [SerializeField] private TMP_Text AT_DotDamage_TMP;
-    [SerializeField] private TMP_Text AT_DotDuration_TMP;
 
     [Header("DropAttackTap")]
     [SerializeField] private RectTransform dropAttackTap;
     [SerializeField] private TMP_Text DropAT_AttackDamage_TMP;
-    [SerializeField] private TMP_Text DropAT_KnockbackPower_TMP;
-    [SerializeField] private TMP_Text DropAT_AirbornePower_TMP;
-    [SerializeField] private TMP_Text DropAT_StunDuration_TMP;
-    [SerializeField] private TMP_Text DropAT_SlowDuration_TMP;
-    [SerializeField] private TMP_Text DropAT_SlowDecrease_TMP;
+    [SerializeField] private TMP_Text DropAT_Knockback_TMP;
+    [SerializeField] private TMP_Text DropAT_Airborne_TMP;
+    [SerializeField] private TMP_Text DropAT_Stun_TMP;
+    [SerializeField] private TMP_Text DropAT_Slow_TMP;
     [SerializeField] private TMP_Text DropAT_DotDamage_TMP;
-    [SerializeField] private TMP_Text DropAT_DotDuration_TMP;
 
     [Header("ChargeAttackTap")]
     [SerializeField] private RectTransform chargeAttackTap;
     [SerializeField] private TMP_Text ChargeAT_AttackDamage_TMP;
-    [SerializeField] private TMP_Text ChargeAT_KnockbackPower_TMP;
-    [SerializeField] private TMP_Text ChargeAT_AirbornePower_TMP;
-    [SerializeField] private TMP_Text ChargeAT_StunDuration_TMP;
-    [SerializeField] private TMP_Text ChargeAT_SlowDuration_TMP;
-    [SerializeField] private TMP_Text ChargeAT_SlowDecrease_TMP;
+    [SerializeField] private TMP_Text ChargeAT_Knockback_TMP;
+    [SerializeField] private TMP_Text ChargeAT_Airborne_TMP;
+    [SerializeField] private TMP_Text ChargeAT_Stun_TMP;
+    [SerializeField] private TMP_Text ChargeAT_Slow_TMP;
     [SerializeField] private TMP_Text ChargeAT_DotDamage_TMP;
-    [SerializeField] private TMP_Text ChargeAT_DotDuration_TMP;
 
     [Header("DashAttackTap")]
     [SerializeField] private RectTransform dashAttackTap;
     [SerializeField] private TMP_Text DashAT_AttackDamage_TMP;
-    [SerializeField] private TMP_Text DashAT_KnockbackPower_TMP;
-    [SerializeField] private TMP_Text DashAT_AirbornePower_TMP;
-    [SerializeField] private TMP_Text DashAT_StunDuration_TMP;
-    [SerializeField] private TMP_Text DashAT_SlowDuration_TMP;
-    [SerializeField] private TMP_Text DashAT_SlowDecrease_TMP;
+    [SerializeField] private TMP_Text DashAT_Knockback_TMP;
+    [SerializeField] private TMP_Text DashAT_Airborne_TMP;
+    [SerializeField] private TMP_Text DashAT_Stun_TMP;
+    [SerializeField] private TMP_Text DashAT_Slow_TMP;
     [SerializeField] private TMP_Text DashAT_DotDamage_TMP;
-    [SerializeField] private TMP_Text DashAT_DotDuration_TMP;
 
     [Header("Taps")]
     [SerializeField] private Button statusTapBtn;
@@ -275,12 +267,10 @@ public class UI_Enhance : MonoBehaviour
                     if (weaponData.baseWeaponLevel >= weaponData.data.enhanceCostList.Count - 1)
                     {
                         levelUpBtn.gameObject.SetActive(false);
-                        selectedCheck.sizeDelta = new Vector2(420f, selectedCheck.sizeDelta.y);
                     }
                     else
                     {
                         levelUpBtn.gameObject.SetActive(true);
-                        selectedCheck.sizeDelta = new Vector2(210f, selectedCheck.sizeDelta.y);
                     }
                 }
                 else
@@ -294,12 +284,10 @@ public class UI_Enhance : MonoBehaviour
                     if (weaponData.baseWeaponLevel >= weaponData.data.enhanceCostList.Count - 1)
                     {
                         levelUpBtn.gameObject.SetActive(false);
-                        selecBtn.GetComponent<RectTransform>().sizeDelta = new Vector2(420f, selecBtn.GetComponent<RectTransform>().sizeDelta.y);
                     }
                     else
                     {
                         levelUpBtn.gameObject.SetActive(true);
-                        selecBtn.GetComponent<RectTransform>().sizeDelta = new Vector2(210f, selecBtn.GetComponent<RectTransform>().sizeDelta.y);
                     }
                 }
             }
@@ -353,64 +341,54 @@ public class UI_Enhance : MonoBehaviour
     {
         UISet(
             AT_AttackDamage_TMP,
-            AT_KnockbackPower_TMP,
-            AT_AirbornePower_TMP,
-            AT_StunDuration_TMP,
-            AT_SlowDuration_TMP,
-            AT_SlowDecrease_TMP,
+            AT_Knockback_TMP,
+            AT_Airborne_TMP,
+            AT_Stun_TMP,
+            AT_Slow_TMP,
             AT_DotDamage_TMP,
-            AT_DotDuration_TMP,
             weaponData.data.baseWeaponEffectList[weaponData.baseWeaponLevel].attackEffect);
     }
     public void DropAttackInfoSet()
     {
         UISet(
             DropAT_AttackDamage_TMP,
-            DropAT_KnockbackPower_TMP,
-            DropAT_AirbornePower_TMP,
-            DropAT_StunDuration_TMP,
-            DropAT_SlowDuration_TMP,
-            DropAT_SlowDecrease_TMP,
+            DropAT_Knockback_TMP,
+            DropAT_Airborne_TMP,
+            DropAT_Stun_TMP,
+            DropAT_Slow_TMP,
             DropAT_DotDamage_TMP,
-            DropAT_DotDuration_TMP,
             weaponData.data.baseWeaponEffectList[weaponData.baseWeaponLevel].dropEffect);
     }
     public void ChargeAttackInfoSet()
     {
         UISet(
             ChargeAT_AttackDamage_TMP,
-            ChargeAT_KnockbackPower_TMP,
-            ChargeAT_AirbornePower_TMP,
-            ChargeAT_StunDuration_TMP,
-            ChargeAT_SlowDuration_TMP,
-            ChargeAT_SlowDecrease_TMP,
+            ChargeAT_Knockback_TMP,
+            ChargeAT_Airborne_TMP,
+            ChargeAT_Stun_TMP,
+            ChargeAT_Slow_TMP,
             ChargeAT_DotDamage_TMP,
-            ChargeAT_DotDuration_TMP,
             weaponData.data.baseWeaponEffectList[weaponData.baseWeaponLevel].chargeEffect);
     }
     public void DashAttackInfoSet()
     {
         UISet(
             DashAT_AttackDamage_TMP,
-            DashAT_KnockbackPower_TMP,
-            DashAT_AirbornePower_TMP,
-            DashAT_StunDuration_TMP,
-            DashAT_SlowDuration_TMP,
-            DashAT_SlowDecrease_TMP,
+            DashAT_Knockback_TMP,
+            DashAT_Airborne_TMP,
+            DashAT_Stun_TMP,
+            DashAT_Slow_TMP,
             DashAT_DotDamage_TMP,
-            DashAT_DotDuration_TMP,
             weaponData.data.baseWeaponEffectList[weaponData.baseWeaponLevel].dashEffect);
     }
-    public void UISet(TMP_Text dmgText, TMP_Text knockbackText, TMP_Text airborneText, TMP_Text stunText, TMP_Text slowDurText, TMP_Text slowDecText, TMP_Text dotDmgText, TMP_Text dotDurText, EffectTypeData data)
+    public void UISet(TMP_Text dmgText, TMP_Text knockbackText, TMP_Text airborneText, TMP_Text stunText, TMP_Text slowDurText, TMP_Text dotDmgText, EffectTypeData data)
     {
         dmgText.text = data.damageMultiple.ToString();
         knockbackText.text = data.knockback.valueA.ToString();
         airborneText.text = data.airborne.valueA.ToString();
         stunText.text = $"{data.stun.valueB}s";
-        slowDurText.text = $"{data.slow.valueA}s";
-        slowDecText.text = $"-{data.slow.valueB}%";
-        dotDmgText.text = data.dotDamage.valueA.ToString();
-        dotDurText.text = $"{data.dotDamage.valueB}s";
+        slowDurText.text = $"{data.slow.valueA}s/-{data.slow.valueB}%";
+        dotDmgText.text = $"{data.dotDamage.valueA}/{data.dotDamage.valueB}s";
     }
     #endregion
 
