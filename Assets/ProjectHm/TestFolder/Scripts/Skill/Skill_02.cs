cff327e724cd40e43d37dccc92769d4ad712518b
MyaMya0709 <chlehdgh96@gmail.com>
2025-10-13 18:05:06 +0900

    [Wip] Skill_02 임시 저장

--- Assets/ProjectHm/TestFolder/Scripts/Skill/Skill_02.cs
+++ Assets/ProjectHm/TestFolder/Scripts/Skill/Skill_02.cs
@@ -3,5 +3,22 @@ using UnityEngine;
 
 public class Skill_02 : ISkill
 {
-    
+
+    public override void SkillRangeCheck()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UseChargeSkill()
+    {
+        UseSkill();
+    }
+
+    public override void UseSkill()
+    {
+        player.
+
+
+        throw new System.NotImplementedException();
+    }
 }
